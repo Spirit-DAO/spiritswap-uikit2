a81ca012e759ccbc6ca57ac9e720b73708404d40
Marcos Iglesias <markkos89@gmail.com>
2021-05-12 19:35:13 -0300

    new icons

---
+++ src/widgets/Menu/icons/Donate.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Svg from "../../../components/Svg/Svg";
+import { SvgProps } from "../../../components/Svg/types";
+
+const Icon: React.FC<SvgProps> = (props) => {
+  return (
+    <Svg viewBox="0 0 16 1" {...props}>
+      <path d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+    </Svg>
+  );
+};
+
+export default Icon;
