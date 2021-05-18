a144ee521c8d9451fe53b556c40dac741884af67
Nico <nicolas.limonoff@gmail.com>
2021-05-18 15:34:03 -0300

    reset button

--- src/components/Button/StyledButton.tsx
+++ src/components/Button/StyledButton.tsx
@@ -58,7 +58,6 @@ const StyledButton = styled.button<BaseButtonProps>`
   opacity: ${getOpacity};
   outline: 0;
   transition: background-color 0.2s;
-  
 
   &:hover:not(:disabled):not(.pancake-button--disabled):not(.pancake-button--disabled):not(:active) {
     opacity: 0.65;
