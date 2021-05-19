bfc7a93dc2432734b24953237bc26d9e1292dfdc
Nico <nicolas.limonoff@gmail.com>
2021-05-19 13:22:08 -0300

    colors

--- src/components/ButtonMenu/StyledButtonMenu.tsx
+++ src/components/ButtonMenu/StyledButtonMenu.tsx
@@ -15,18 +15,8 @@ const StyledButtonMenu = styled.div<{ variant: Variant }>`
   border-radius: 16px;
   display: inline-flex;
   border-radius: 0.5rem;
-  
+
   & > button + button,
-  & > button {
-    color: ${({ theme }) => (theme.isDark ? `#fff` : `#0ad9e4`)};
-  box-shadow: 0 0 6px 0 rgba(157, 96, 212, 0.5);
-  border: solid 3px transparent;
-  background-image: linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)),
-    linear-gradient(90deg, #42d784, #60d5dc);
-  background-origin: border-box;
-  box-shadow: ${({ theme }) => (theme.isDark ? `2px 1000px 1px #212230 inset` : `2px 1000px 1px #E6FDFE inset`)};
-  background-clip: content-box, border-box;
-  }
   & > a + a {
     margin-left: 2px; // To avoid focus shadow overlap
   }
