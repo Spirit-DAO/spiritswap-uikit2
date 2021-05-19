5f3f372770b5571d247da187313d0f4fccbf5e51
Nico <nicolas.limonoff@gmail.com>
2021-05-19 13:16:05 -0300

    colors

--- dist/components/ButtonMenu/StyledButtonMenu.d.ts
+++ dist/components/ButtonMenu/StyledButtonMenu.d.ts
@@ -1,3 +1,6 @@
 import { DefaultTheme } from "styled-components";
-declare const StyledButtonMenu: import("styled-components").StyledComponent<"div", DefaultTheme, {}, never>;
+import { Variant } from "../Button/types";
+declare const StyledButtonMenu: import("styled-components").StyledComponent<"div", DefaultTheme, {
+    variant: Variant;
+}, never>;
 export default StyledButtonMenu;
--- src/components/ButtonMenu/StyledButtonMenu.tsx
+++ src/components/ButtonMenu/StyledButtonMenu.tsx
@@ -10,11 +10,15 @@ const getBackgroundColor = ({ theme, variant }: StyledButtonMenuProps) => {
   return theme.colors[variant === variants.SUBTLE ? "input" : "tertiary"];
 };
 
-const StyledButtonMenu = styled.div`
+const StyledButtonMenu = styled.div<{ variant: Variant }>`
+  //background-color: ${getBackgroundColor};
   border-radius: 16px;
   display: inline-flex;
   border-radius: 0.5rem;
-  color: ${({ theme }) => (theme.isDark ? `#fff` : `#0ad9e4`)};
+  
+  & > button + button,
+  & > button {
+    color: ${({ theme }) => (theme.isDark ? `#fff` : `#0ad9e4`)};
   box-shadow: 0 0 6px 0 rgba(157, 96, 212, 0.5);
   border: solid 3px transparent;
   background-image: linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)),
@@ -22,8 +26,7 @@ const StyledButtonMenu = styled.div`
   background-origin: border-box;
   box-shadow: ${({ theme }) => (theme.isDark ? `2px 1000px 1px #212230 inset` : `2px 1000px 1px #E6FDFE inset`)};
   background-clip: content-box, border-box;
-
-  & > button + button,
+  }
   & > a + a {
     margin-left: 2px; // To avoid focus shadow overlap
   }
