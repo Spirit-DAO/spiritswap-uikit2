7efe629ca7eb1ee098a7ee9f1849e4ec21549fa3
Nico <nicolas.limonoff@gmail.com>
2021-05-19 12:48:02 -0300

    colors

--- src/components/ButtonMenu/StyledButtonMenu.tsx
+++ src/components/ButtonMenu/StyledButtonMenu.tsx
@@ -11,16 +11,16 @@ const getBackgroundColor = ({ theme, variant }: StyledButtonMenuProps) => {
 };
 
 const StyledButtonMenu = styled.div<{ variant: Variant }>`
-    border-radius: 0.5rem;
-    color: ${({ theme }) => (theme.isDark ? `#fff` : `#0ad9e4`)};
-    box-shadow: 0 0 6px 0 rgba(157, 96, 212, 0.5);
-    border: solid 3px transparent;
-    background-image: linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)),
-      linear-gradient(90deg, #42d784, #60d5dc);
-    background-origin: border-box;
-    box-shadow: ${getBackgroundColor};
-    background-clip: content-box, border-box;
-  display: inline-flex; 
+  border-radius: 0.5rem;
+  color: ${({ theme }) => (theme.isDark ? `#fff` : `#0ad9e4`)};
+  box-shadow: 0 0 6px 0 rgba(157, 96, 212, 0.5);
+  border: solid 3px transparent;
+  background-image: linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)),
+    linear-gradient(90deg, #42d784, #60d5dc);
+  background-origin: border-box;
+  box-shadow: ${getBackgroundColor};
+  background-clip: content-box, border-box;
+  display: inline-flex;
   & > button,
   & > a + a {
     margin-left: 2px; // To avoid focus shadow overlap
