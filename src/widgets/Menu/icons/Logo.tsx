2a3549fc21e541d8332290b95f90b61dcc72f60a
pablo <pablo.munaretto@gmail.com>
2021-05-27 17:36:14 -0300

    menu/icons/logo.tsx changed

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -20,8 +20,7 @@ const IconContainer = styled.div`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  /* height: 50px;
-  background: red; */
+  
   /* & > img {
     width: 60px;
     align-items: center;
@@ -43,8 +42,8 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
     <IconContainer>
       <img {...props} src="/images/spiritswap_logo.png" alt="" width="80" height="116" />
 
-      <span style={{ color: "#52D784", letterSpacing: "3px" }}>
-        Spirit<span style={{ color: "#60D5DC" }}>Swap</span>
+      <span {...props} style={{ color: "#52D784", letterSpacing: "3px" }}>
+        Spirit<span {...props} style={{ color: "#60D5DC" }}>Swap</span>
       </span>
     </IconContainer>
   );
