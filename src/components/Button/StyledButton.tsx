6c3c729150ac1b1012304427e9545eb191f19e7d
Nico <nicolas.limonoff@gmail.com>
2021-05-28 11:15:29 -0300

    unlock button

--- src/components/Button/StyledButton.tsx
+++ src/components/Button/StyledButton.tsx
@@ -46,7 +46,7 @@ const StyledButton = styled.button<BaseButtonProps>`
   align-items: center;
   /* border: 0;
   border-radius: 0.5rem;
-  box-shadow: 0px -1px 0px 0px rgba(14, 14, 44, 0.4) inset;
+  box-shadow: 0px -1px 0px 0px rgba(14, 14, 44, 0.4) inset; */
   cursor: pointer;
   display: inline-flex;
   font-family: inherit;
@@ -55,19 +55,27 @@ const StyledButton = styled.button<BaseButtonProps>`
   justify-content: center;
   letter-spacing: 0.03em;
   line-height: 1;
-  opacity: ${getOpacity}; */
-  /* outline: 0;
-  transition: background-color 0.2s; */
+  opacity: ${getOpacity};
+  outline: 0;
+  transition: background-color 0.2s; 
 
   border-radius: 0.5rem;
-  color: ${({ theme }) => (theme.isDark ? `#fff` : `#0ad9e4`)};
-  box-shadow: 0 0 6px 0 rgba(157, 96, 212, 0.5);
+  //color: ${({ theme }) => (theme.isDark ? `#fff` : `#0ad9e4`)};
+
   border: solid 3px transparent;
   background-image: linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)),
     linear-gradient(90deg, #42d784, #60d5dc);
   background-origin: border-box;
   background-clip: content-box, border-box;
-  box-shadow: ${({ theme }) => (theme.isDark ? `2px 1000px 1px #212230 inset` : `2px 1000px 1px #E6FDFE inset`)};
+  ${(props) =>
+    !props.disabled
+      ? `
+    box-shadow: 2px 1000px 1px #212230 inset; `
+      : ` 
+    box-shadow: 2px 1000px 1px rgb(66,64,55) inset;
+    color: rgb(113,113,97);
+    background-image: none;
+    `}
 
   &:hover:not(:disabled):not(.pancake-button--disabled):not(.pancake-button--disabled):not(:active) {
     opacity: 0.65;
