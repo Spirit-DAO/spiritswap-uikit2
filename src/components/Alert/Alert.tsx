55d91df09609fe4694621fe4f377e4b27360aba2
Brad <bradleyhong37@gmail.com>
2021-07-19 17:00:07 -0500

    set the border radius of the alert component

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -46,7 +46,7 @@ const getIcon = (variant: AlertProps["variant"] = variants.INFO) => {
 
 const IconLabel = styled.div<ThemedIconLabel>`
   background-color: ${getThemeColor};
-  border-radius: 16px 0 0 16px;
+  border-radius: 0.5rem 0 0 0.5rem;
   color: ${({ theme }) => theme.alert.background};
   padding: 12px;
 `;
@@ -70,7 +70,6 @@ const CloseHandler = styled.div`
 const StyledAlert = styled(Flex)`
   position: relative;
   background-color: ${({ theme }) => theme.alert.background};
-  //border-radius: 16px;
   border-radius: 0.5rem;
   box-shadow: 0px 20px 36px -8px rgba(14, 14, 44, 0.1), 0px 1px 1px rgba(0, 0, 0, 0.05);
 `;
