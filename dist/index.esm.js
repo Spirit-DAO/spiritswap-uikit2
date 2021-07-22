4d1d0eb2eb6f00d3078fe902e905cc64fca29e78
toiaa <victoria@geddes.com.ar>
2021-07-22 16:46:41 -0300

    trying svg

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2298,11 +2298,11 @@ var Icon$1d = function (props) {
 };
 
 var Icon$1e = function (props) {
-    return (React.createElement("svg", { width: "24", height: "24", viewBox: "0 0 24 24", fill: "none", xmlns: "http://www.w3.org/2000/svg" },
+    return (React.createElement(Svg, { width: "26px", height: "26px", viewBox: "0 0 26 26", fill: "none", xmlns: "http://www.w3.org/2000/svg" },
         React.createElement("circle", { cx: "13", cy: "13", r: "12", stroke: "#52D784", "stroke-width": "2", "stroke-linejoin": "round" }),
         React.createElement("path", { d: "M20 13C20 12.0807 19.8189 11.1705 19.4672 10.3212C19.1154 9.47194 18.5998 8.70026 17.9497 8.05025C17.2997 7.40024 16.5281 6.88463 15.6788 6.53284C14.8295 6.18106 13.9193 6 13 6C12.0807 6 11.1705 6.18106 10.3212 6.53284C9.47194 6.88463 8.70026 7.40024 8.05025 8.05025C7.40024 8.70026 6.88463 9.47194 6.53284 10.3212C6.18106 11.1705 6 12.0807 6 13", stroke: "#52D784", "stroke-width": "2", "stroke-linecap": "round", "stroke-linejoin": "round" }),
         React.createElement("path", { d: "M13 10L13 14", stroke: "#52D784", "stroke-width": "2", "stroke-linecap": "round", "stroke-linejoin": "round" }),
-        React.createElement("circle", { cx: "13", cy: "17", r: "3" })));
+        React.createElement("circle", { cx: "13", cy: "17", r: "3", fill: "#1A243C" })));
 };
 
 var IconModule = /*#__PURE__*/Object.freeze({
