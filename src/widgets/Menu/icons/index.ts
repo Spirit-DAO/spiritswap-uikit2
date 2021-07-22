b0a6984fe16d4e44f6c8a00ceb82b677cd224544
toiaa <victoria@geddes.com.ar>
2021-07-22 16:20:32 -0300

    gauge icon added

--- src/widgets/Menu/icons/index.ts
+++ src/widgets/Menu/icons/index.ts
@@ -28,3 +28,4 @@ export { default as StoreIcon } from "./Store";
 export { default as AnalyticIcon } from "./Analytic";
 export { default as DonateIcon } from "./Donate";
 export { default as BadgeNewIcon } from "./BadgeNew";
+export { default as Gauge } from "./Gauge";
