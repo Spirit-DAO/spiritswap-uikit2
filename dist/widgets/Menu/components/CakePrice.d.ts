d1529f736d2176a7c80f5ddfbdd02d1adc7e2714
Brad <bradleyhong37@gmail.com>
2021-12-01 21:36:28 -0600

    display the fantom price

--- dist/widgets/Menu/components/CakePrice.d.ts
+++ dist/widgets/Menu/components/CakePrice.d.ts
@@ -1,5 +1,10 @@
 import React from "react";
+export declare enum Token {
+    SPIRIT = "SPIRIT",
+    FTM = "FTM"
+}
 interface Props {
+    token?: Token;
     cakePriceUsd?: number;
 }
 declare const _default: React.NamedExoticComponent<Props>;
--- src/widgets/Menu/types.ts
+++ src/widgets/Menu/types.ts
@@ -46,6 +46,7 @@ export interface PanelProps {
   isDark: boolean;
   toggleTheme: (isDark: boolean) => void;
   cakePriceUsd?: number;
+  ftmPriceUsd?: number;
   currentLang: string;
   langs: LangType[];
   setLang: (lang: LangType) => void;
