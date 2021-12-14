4b8e02587db011c2475ecb15e6c8b5f355550371
Nicolas <nlucas120@gmail.com>
2021-12-14 15:33:30 -0300

    new config on mobile for wallets

--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -6,33 +6,67 @@ import WalletConnect from "./icons/WalletConnect";
 import TrustWallet from "./icons/TrustWallet";
 import { Config, ConnectorNames } from "./types";
 
-const connectors: Config[] = [
-  {
-    title: "MetaMask",
-    icon: Metamask,
-    connectorId: ConnectorNames.Injected,
-  },
-  {
-    title: "Coin98 Wallet",
-    icon: Coin98Wallet,
-    connectorId: ConnectorNames.Coin98,
-  },
-  {
-    title: "Coinbase Wallet",
-    icon: Coinbase,
-    connectorId: ConnectorNames.WalletLink,
-  },
-  {
-    title: "Wallet Connect",
-    icon: WalletConnect,
-    connectorId: ConnectorNames.WalletConnect,
-  },
-  {
-    title: "Trust Connect",
-    icon: TrustWallet,
-    connectorId: ConnectorNames.TrustWallet,
-  },
-];
+const Connectors = () => {
+  const { isXl } = useMatchBreakpoints();
+  const isMobile = isXl === false;
 
-export default connectors;
+  let connectors: Config[];
+
+  if (isMobile) {
+    connectors = [
+      {
+        title: "MetaMask",
+        icon: Metamask,
+        connectorId: ConnectorNames.Injected,
+      },
+      {
+        title: "Coin98 Wallet",
+        icon: Coin98Wallet,
+        connectorId: ConnectorNames.Coin98,
+      },
+      {
+        title: "Coinbase Wallet",
+        icon: Coinbase,
+        connectorId: ConnectorNames.WalletLink,
+      },
+      {
+        title: "Wallet Connect",
+        icon: WalletConnect,
+        connectorId: ConnectorNames.WalletConnect,
+      },
+      {
+        title: "Trust Connect",
+        icon: TrustWallet,
+        connectorId: ConnectorNames.Injected,
+      },
+    ];
+  } else {
+    connectors = [
+      {
+        title: "MetaMask",
+        icon: Metamask,
+        connectorId: ConnectorNames.Injected,
+      },
+      {
+        title: "Coin98 Wallet",
+        icon: Coin98Wallet,
+        connectorId: ConnectorNames.Coin98,
+      },
+      {
+        title: "Coinbase Wallet",
+        icon: Coinbase,
+        connectorId: ConnectorNames.WalletLink,
+      },
+      {
+        title: "Wallet Connect",
+        icon: WalletConnect,
+        connectorId: ConnectorNames.WalletConnect,
+      },
+    ];
+  }
+
+  return connectors;
+};
+
+export default Connectors;
 export const connectorLocalStorageKey = "connectorId";
